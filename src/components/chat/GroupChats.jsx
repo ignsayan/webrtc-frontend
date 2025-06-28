45a85222c232d84c863e80b28f9eed90483ba624
Sayan Karmakar <102944275+ignsayan@users.noreply.github.com>
2025-06-28 10:47:47 +0530

    added group filter

--- src/components/chat/GroupChats.jsx
+++ src/components/chat/GroupChats.jsx
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function GroupChats({
     openInbox,
     recents,
 }) {
 
+    const [chats, setChats] = useState([]);
+
+    useEffect(() => {
+        const chats = recents.filter((recent) => recent.isGroup);
+        setChats(chats);
+    }, [recents]);
+    
     return (
         <>
-
+            {chats && chats.map((chat, index) => (
+                <li
+                    key={index} onClick={() => openInbox(chat.participants[0]?._id)}
+                    className="flex items-center gap-3 p-3 bg-gray-700 rounded-full hover:bg-gray-600 cursor-pointer shadow-xl"
+                >
+                    <img
+                        src={`https://ui-avatars.com/api/?name=${chat.participants[0]?.first_name}+${chat.participants[0]?.last_name}&background=random`}
+                        className="w-10 h-10 rounded-full object-cover shadow-md"
+                    />
+                    <div className="flex-1">
+                        <div className="font-medium">
+                            {chat.participants[0]?.first_name} {chat.participants[0]?.last_name}
+                        </div>
+                        <div className="text-sm text-gray-400 truncate">
+                            {chat.message?.content?.length > 15
+                                ? `${chat.message.content.substring(0, 18)} ...`
+                                : chat.message?.content || 'No messages yet'}
+                        </div>
+                    </div>
+                </li>
+            ))}
         </>
     );
 }
--- src/components/chat/PersonalChats.jsx
+++ src/components/chat/PersonalChats.jsx
@@ -1,28 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function PersonalChats({
     openInbox,
     recents,
 }) {
 
+    const [chats, setChats] = useState([]);
+
+    useEffect(() => {
+        const chats = recents.filter((recent) => !recent.isGroup)
+            .map((recent) => {
+                const { participants, ...rest } = recent;
+                const [user] = participants;
+                return { ...rest, user };
+            });
+        setChats(chats);
+    }, [recents]);
+
     return (
         <>
-            {recents.map((recent, index) => (
+            {chats && chats.map((chat, index) => (
                 <li
-                    key={index} onClick={() => openInbox(recent.participants[0]?._id)}
+                    key={index} onClick={() => openInbox(chat.user._id)}
                     className="flex items-center gap-3 p-3 bg-gray-700 rounded-full hover:bg-gray-600 cursor-pointer shadow-xl"
                 >
                     <img
-                        src={recent.participants[0]?.avatar || `https://ui-avatars.com/api/?name=${recent.participants[0]?.first_name}+${recent.participants[0]?.last_name}&background=random`} className="w-10 h-10 rounded-full object-cover shadow-md"
+                        src={chat.user.avatar || `https://ui-avatars.com/api/?name=${chat.user.first_name}+${chat.user.last_name}&background=random`} className="w-10 h-10 rounded-full object-cover shadow-md"
                     />
                     <div className="flex-1">
                         <div className="font-medium">
-                            {`${recent.participants[0]?.first_name} ${recent.participants[0]?.last_name}`}
+                            {`${chat.user.first_name} ${chat.user.last_name}`}
                         </div>
                         <div className="text-sm text-gray-400 truncate">
-                            {recent.message?.content?.length > 15
-                                ? `${recent.message.content.substring(0, 18)} ...`
-                                : recent.message?.content || 'No messages yet'}
+                            {chat.message?.content?.length > 15
+                                ? `${chat.message.content.substring(0, 18)} ...`
+                                : chat.message?.content || 'No messages yet'}
                         </div>
                     </div>
                 </li>
